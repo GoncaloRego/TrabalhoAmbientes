2d4efc1bf335d7517c096940b5fd8ad156dac435
MarioCaldas <caldas_96@live.com.pt>
2017-01-10 23:36:06 +0000

    SpawnAndPathFinder

---
+++ Assets/Scripts/EnemySpwan.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpwan : MonoBehaviour {
+
+
+    
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+
+    // Use this for initialization
+    void Start () {
+
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+
+    }
+
+    // Update is called once per frame
+    void Spawn() { 
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    
+
+}
+}
---
+++ Assets/Scripts/NewEnemyMov.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewEnemyMov : MonoBehaviour {
+
+
+    Transform player;               // Reference to the player's position.
+    
+    NavMeshAgent nav;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        nav = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(player.position);
+    }
+}
